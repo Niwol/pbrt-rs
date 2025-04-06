4d9aea1f3a490da47d984548fab576d7caf64781
Niwol <lowin000@gmail.com>
2025-04-06 13:26:27 +0200

    Start integrator interface

--- src/cpu/integrator.rs
+++ src/cpu/integrator.rs
@@ -1,1 +1,19 @@
+use crate::pbrt::Float;
 
+pub trait Integrator {
+    fn to_string(&self) -> String;
+    fn tr(&self /*, p0: &Interaction, p1: &Interaction, lambda: SampledWavelength */)
+    /* -> SampledSpectrum */;
+}
+
+pub trait Renderer {
+    fn render(&self);
+}
+
+pub trait Intersector {
+    fn intersect(&self /*, ray: &Ray */, t_max: Float) /* -> Option<ShapeIntersection> */;
+    fn intersect_p(&self /*, ray: &Ray */, t_max: Float) -> bool;
+    fn unoccluded(&self /*, p0: Interaction, p1: Interaction */) -> bool {
+        false
+    }
+}
